fbca814421766cca08c35f8bffe5da40467c2664
ahaurw01 <aaron.haurwitz@gmail.com>
2016-02-21 16:37:03 -0500

    add card list

---
+++ src/data/cards.js
@@ -0,0 +1,328 @@
+export default [{
+  name: "Academy at Tolaria West",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/academy-at-tolaria-west.jpg",
+},
+{
+  name: "Agyrem",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/agyrem.jpg",
+},
+{
+  name: "Akoum",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/akoum.jpg",
+},
+{
+  name: "Aretopolis",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/aretopolis.jpg",
+},
+{
+  name: "Astral Arena",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/astral-arena.jpg",
+},
+{
+  name: "Bant",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/bant.jpg",
+},
+{
+  name: "Bloodhill Bastion",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/bloodhill-bastion.jpg",
+},
+{
+  name: "Cliffside Market",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/cliffside-market.jpg",
+},
+{
+  name: "Edge of Malacol",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/edge-of-malacol.jpg",
+},
+{
+  name: "Eloren Wilds",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/eloren-wilds.jpg",
+},
+{
+  name: "Feeding Grounds",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/feeding-grounds.jpg",
+},
+{
+  name: "Fields of Summer",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/fields-of-summer.jpg",
+},
+{
+  name: "Furnace Layer",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/furnace-layer.jpg",
+},
+{
+  name: "Gavony",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/gavony.jpg",
+},
+{
+  name: "Glen Elendra",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/glen-elendra.jpg",
+},
+{
+  name: "Glimmervoid Basin",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/glimmervoid-basin.jpg",
+},
+{
+  name: "Goldmeadow",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/goldmeadow.jpg",
+},
+{
+  name: "Grand Ossuary",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/grand-ossuary.jpg",
+},
+{
+  name: "Grixis",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/grixis.jpg",
+},
+{
+  name: "Grove of the Dreampods",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/grove-of-the-dreampods.jpg",
+},
+{
+  name: "Hedron Fields of Agadeem",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/hedron-fields-of-agadeem.jpg",
+},
+{
+  name: "Immersturm",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/immersturm.jpg",
+},
+{
+  name: "Isle of Vesuva",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/isle-of-vesuva.jpg",
+},
+{
+  name: "Izzet Steam Maze",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/izzet-steam-maze.jpg",
+},
+{
+  name: "Jund",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/jund.jpg",
+},
+{
+  name: "Kessig",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/kessig.jpg",
+},
+{
+  name: "Kharasha Foothills",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/kharasha-foothills.jpg",
+},
+{
+  name: "Kilnspire District",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/kilnspire-district.jpg",
+},
+{
+  name: "Krosa Steven Belledin",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/krosa-steven-belledin.jpg",
+},
+{
+  name: "Lair of the Ashen Idol",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/lair-of-the-ashen-idol.jpg",
+},
+{
+  name: "Lethe Lake",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/lethe-lake.jpg",
+},
+{
+  name: "Llanowar",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/llanowar.jpg",
+},
+{
+  name: "Minamo",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/minamo.jpg",
+},
+{
+  name: "Mount Keralia",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/mount-keralia.jpg",
+},
+{
+  name: "Murasa",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/murasa.jpg",
+},
+{
+  name: "Naar Isle",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/naar-isle.jpg",
+},
+{
+  name: "Naya",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/naya.jpg",
+},
+{
+  name: "Nephalia",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/nephalia.jpg",
+},
+{
+  name: "Norn's Dominion",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/norn's-dominion.jpg",
+},
+{
+  name: "Onakke Catacomb",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/onakke-catacomb.jpg",
+},
+{
+  name: "Orochi Colony",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/orochi-colony.jpg",
+},
+{
+  name: "Orzhova",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/orzhova.jpg",
+},
+{
+  name: "Otaria",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/otaria.jpg",
+},
+{
+  name: "Panopticon",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/panopticon.jpg",
+},
+{
+  name: "Pools of Becoming",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/pools-of-becoming.jpg",
+},
+{
+  name: "Prahv",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/prahv.jpg",
+},
+{
+  name: "Quicksilver Sea",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/quicksilver-sea.jpg",
+},
+{
+  name: "Raven's Run",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/raven's-run.jpg",
+},
+{
+  name: "Sanctum of Serra",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/sanctum-of-serra.jpg",
+},
+{
+  name: "Sea of Sand",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/sea-of-sand.jpg",
+},
+{
+  name: "Selesnya Loft Gardens",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/selesnya-loft-gardens.jpg",
+},
+{
+  name: "Shiv",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/shiv.jpg",
+},
+{
+  name: "Skybreen",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/skybreen.jpg",
+},
+{
+  name: "Sokenzan",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/sokenzan.jpg",
+},
+{
+  name: "Stairs to Infinity",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/stairs-to-infinity.jpg",
+},
+{
+  name: "Stensia",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/stensia.jpg",
+},
+{
+  name: "Stronghold Furnace",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/stronghold-furnace.jpg",
+},
+{
+  name: "Takenuma",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/takenuma.jpg",
+},
+{
+  name: "Talon Gates",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/talon-gates.jpg",
+},
+{
+  name: "Tazeem",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/tazeem.jpg",
+},
+{
+  name: "The Æther Flues",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/the-æther-flues.jpg",
+},
+{
+  name: "The Dark Barony",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/the-dark-barony.jpg",
+},
+{
+  name: "The Eon Fog",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/the-eon-fog.jpg",
+},
+{
+  name: "The Fourth Sphere",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/the-fourth-sphere.jpg",
+},
+{
+  name: "The Great Forest",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/the-great-forest.jpg",
+},
+{
+  name: "The Hippodrome",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/the-hippodrome.jpg",
+},
+{
+  name: "The Maelstrom",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/the-maelstrom.jpg",
+},
+{
+  name: "The Zephyr Maze",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/the-zephyr-maze.jpg",
+},
+{
+  name: "Trail of the Mage-Rings",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/trail-of-the-mage-rings.jpg",
+},
+{
+  name: "Truga Jungle",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/truga-jungle.jpg",
+},
+{
+  name: "Turri Island",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/turri-island.jpg",
+},
+{
+  name: "Undercity Reaches",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/undercity-reaches.jpg",
+},
+{
+  name: "Velis Vel",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/velis-vel.jpg",
+},
+{
+  name: "Windriddle Palaces",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/windriddle-palaces.jpg",
+},
+{
+  name: "Chaotic Æther",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/chaotic-æther.jpg",
+},
+{
+  name: "Interplanar Tunnel",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/interplanar-tunnel.jpg",
+},
+{
+  name: "Morphic Tide",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/morphic-tide.jpg",
+},
+{
+  name: "Mutual Epiphany",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/mutual-epiphany.jpg",
+},
+{
+  name: "Planewide Disaster",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/planewide-disaster.jpg",
+},
+{
+  name: "Reality Shaping",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/reality-shaping.jpg",
+},
+{
+  name: "Spatial Merging",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/spatial-merging.jpg",
+},
+{
+  name: "Time Distortion",
+  imageUrl: "http://magiccards.info/extras/plane/planechase/time-distortion.jpg",
+}]

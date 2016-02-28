002de75cf7a9cd6a9bbf84672c667d02eaef6c53
ahaurw01 <aaron.haurwitz@gmail.com>
2016-02-28 18:48:39 -0500

    add die roll markup and styling

--- src/components/die.jsx
+++ src/components/die.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import SpinningDie from './spinning-die'
 
-const Die = ({ roll, face }) =>
-  <button className="game-die" onClick={roll}>
-    {face}
-  </button>
+const Die = ({ roll, face }) => {
+  let innards
+  if (true) { //(face === 'rolling') {
+    innards = (<SpinningDie />)
+  } else {
+    innards = (
+      <button className="" onClick={roll}>
+        {face}
+      </button>
+    )
+  }
+  return (
+    <div className="game-die">
+      {innards}
+    </div>
+  )
+}
+
 
 Die.propTypes = {
   roll: React.PropTypes.func.isRequired,
---
+++ src/components/spinning-die.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SpinningDie = () =>
+  <div className="game-die-spinning">
+    <div className="cube">
+      <div className="cube-front">front</div>
+      <div className="cube-back">back</div>
+      <div className="cube-top">top</div>
+      <div className="cube-bottom">bottom</div>
+      <div className="cube-left">left</div>
+      <div className="cube-right">right</div>
+    </div>
+  </div>
+
+export default SpinningDie

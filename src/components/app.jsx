506b64ee31d05501a7af909186d513926bf1b3e7
ahaurw01 <aaron.haurwitz@gmail.com>
2016-02-28 12:27:48 -0500

    make svg icon a component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router'
+import PlanechaseIcon from './planechase-icon'
 
 const App = ({children}) =>
   <div className="container">
-    <h1>
-      <img
-        className="header-image"
-        src="images/planechase-icon.svg" />
-      <span className="header-text">Play Planechase</span>
-    </h1>
+    <Link to="/" className="header-link">
+      <h1>
+        <div className="header-image">
+          <PlanechaseIcon />
+        </div>
+        <span className="header-text">Play Planechase</span>
+      </h1>
+    </Link>
     <div>
       {children}
     </div>

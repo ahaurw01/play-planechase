d2a12fbba68b7116ac987d9e5df7355d385f53d4
ahaurw01 <aaron.haurwitz@gmail.com>
2016-02-28 19:22:49 -0500

    put planar symbols on the die

--- src/components/spinning-die.jsx
+++ src/components/spinning-die.jsx
@@ -3,12 +3,16 @@ import React from 'react'
 const SpinningDie = () =>
   <div className="game-die-spinning">
     <div className="cube">
-      <div className="cube-front">front</div>
-      <div className="cube-back">back</div>
-      <div className="cube-top">top</div>
-      <div className="cube-bottom">bottom</div>
-      <div className="cube-left">left</div>
-      <div className="cube-right">right</div>
+      <div className="cube-front cube-blank"></div>
+      <div className="cube-back cube-blank"></div>
+      <div className="cube-top cube-blank"></div>
+      <div className="cube-bottom cube-blank"></div>
+      <div className="cube-left">
+        <div className="cube-symbol cube-chaos" />
+      </div>
+      <div className="cube-right">
+        <div className="cube-symbol cube-walk" />
+      </div>
     </div>
   </div>
 
